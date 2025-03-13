ef82d8089b28358dbac45fd4c87d883469c92043
David Balzer <dmbalzer@gmail.com>
2025-03-13 08:24:24 -0400

    removed stb_ds due to errors

--- src/main.c
+++ src/main.c
@@ -1,13 +1,8 @@
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
-#define STB_DS_IMPLEMENTATION
-#include "stb_ds.h"
 #include "structs.h"
 
 bool quit = false;
-
-Texture* textures = NULL;
-Gif* gifs = NULL;
 
 int main(void)
 {
@@ -21,34 +16,22 @@ int main(void)
 	FilePathList fpl = LoadDirectoryFiles("./assets");
 	for ( int i = 0; i < fpl.count; i++ ) {
 		if ( IsFileExtension(fpl.paths[i], ".png") ) {
-			arrput(textures, LoadTexture(fpl.paths[i]));
+
 		} else if ( IsFileExtension(fpl.paths[i], ".gif") ) {
-			Gif gif = { 0 };
-			gif.image = LoadImageAnim(fpl.paths[i], &gif.qty);
-			Texture texture = LoadTextureFromImage(gif.image);
-			gif.texture = &texture;
-			arrput(textures, texture);
-			arrput(gifs, gif);
+			/* TODO */
 		}
 	}
 	UnloadDirectoryFiles(fpl);
 	
 	while ( !quit ) {
 		if ( WindowShouldClose() ) quit = true;
+
 		BeginDrawing();
 		ClearBackground(DARKGRAY);
-		int x = 0, y = 0;	
-		for ( int i = 0; i < arrlen(textures); i++ ) {
-			DrawTexture(textures[i], x, y, WHITE);
-			x += textures[i].width;
-		}
 
 		EndDrawing();
 	}
 
-	for ( int i = 0; i < arrlen(textures); i++ ) {
-		UnloadTexture(textures[i]);
-	}
 	UnloadFont(font);
 	CloseWindow();
 	return 0;
